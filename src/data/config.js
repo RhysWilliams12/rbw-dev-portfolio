9485f0fd1ecc1a727b6fa35d584c20b689d8b506
RhysWilliams12 <rhyscmn@live.co.uk>
2020-07-15 18:31:06 +0100

    personalised gatsby-portfolio-dev with personal social links and used name

---
+++ src/data/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  defaultTitle: 'Rhys Williams',
+  logo: 'https://rbw-portfolio.com/favicon/favicon-512.png',
+  author: 'Rhys Williams',
+  url: 'https://rbw-portfolio.com',
+  legalName: 'Rhys Bell Williams',
+  defaultDescription: 'I’m Rhys and I’m a full stack engineer!',
+  socialLinks: {
+    twitter: 'https://twitter.com/rhys6052',
+    github: 'https://github.com/RhysWilliams12',
+    linkedin: 'https://www.linkedin.com/in/rhys-williams-38a5019b/',
+  },
+  googleAnalyticsID: 'UA',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@rhys6052',
+  },
+  address: {
+    city: 'Cardiff',
+    region: 'Wales',
+    country: 'United Kingdom',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'rhys6052@gmail.com',
+    phone: '+44',
+  },
+  foundingDate: '2020',
+};
